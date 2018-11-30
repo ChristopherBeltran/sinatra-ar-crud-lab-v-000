7da9d06382c5068dd6a03d30881571a93e37fe9b
Christopher Beltran <christod.beltran@gmail.com>
2018-11-30 03:53:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require_relative '../models/article.rb'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,7 +9,29 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
   end
-end
+
+
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    redirect to '/posts'
+  end
+  
+  
+  get 'articles/new' do
+    erb :'views/new'
+  end 
+  
+  post '/articles' do
+    @article = Article.new(params)
+  end 
+    
+end 
+#>>>>>>> 9c6464d9607186bb92210f4fcc4f928725d5ad16
